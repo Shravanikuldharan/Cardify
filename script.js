96149a44422f4554f7394c671dc23e7e9d2f5f94
Shravanikuldharan <shravanikuldharan05@gmail.com>
2025-06-21 22:01:09 +0530

    Logic changed

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-const theme = document.querySelector(':root');
-const btns = document.querySelectorAll('.btn');
+const theme = document.documentElement;
 
-btns.forEach(function(btn){
-
-    btn.addEventListener("click", function(e){
-
-        const color = e.currentTarget.classList;
-
-        if(color.contains("btn1")){
-            theme.style.setProperty("--theme-color", "#3498db");
-        }
-        else if(color.contains("btn2")){
-            theme.style.setProperty("--theme-color", "#ff1756");
-        }
-        else if(color.contains("btn3")){
-            theme.style.setProperty("--theme-color", "#1cb65d");
-        }
-        else if(color.contains("btn4")){
-            theme.style.setProperty("--theme-color", "#8e44ad");
-        }
-        else{
-            theme.style.setProperty("--theme-color", "#f4b932");
-        }
-    });
+document.querySelectorAll('.btn').forEach(btn => {
+  btn.addEventListener('click', () => {
+    if (btn.classList.contains('btn1')) {
+      theme.style.setProperty('--theme-color', '#3498db');
+    } else if (btn.classList.contains('btn2')) {
+      theme.style.setProperty('--theme-color', '#ff1756');
+    } else if (btn.classList.contains('btn3')) {
+      theme.style.setProperty('--theme-color', '#1cb65d');
+    } else if (btn.classList.contains('btn4')) {
+      theme.style.setProperty('--theme-color', '#8e44ad');
+    } else {
+      theme.style.setProperty('--theme-color', '#f4b932');
+    }
+  });
 });
