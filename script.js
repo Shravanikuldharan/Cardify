71d8f2e10298c9fa92f4516bdff8ca9e233984a0
Shravanikuldharan <shravanikuldharan05@gmail.com>
2025-06-25 19:21:49 +0530

    Logic changes

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 const theme = document.documentElement;
 
-document.querySelectorAll('.btn').forEach(btn => {
-  btn.addEventListener('click', () => {
-    if (btn.classList.contains('btn1')) {
-      theme.style.setProperty('--theme-color', '#3498db');
-    } else if (btn.classList.contains('btn2')) {
-      theme.style.setProperty('--theme-color', '#ff1756');
-    } else if (btn.classList.contains('btn3')) {
-      theme.style.setProperty('--theme-color', '#1cb65d');
-    } else if (btn.classList.contains('btn4')) {
-      theme.style.setProperty('--theme-color', '#8e44ad');
-    } else {
-      theme.style.setProperty('--theme-color', '#f4b932');
-    }
-  });
-});
+const buttons = document.getElementsByClassName('btn');
+
+buttons[0].onclick = function () {
+  theme.style.setProperty('--theme-color', '#3498db');
+};
+buttons[1].onclick = function () {
+  theme.style.setProperty('--theme-color', '#ff1756');
+};
+buttons[2].onclick = function () {
+  theme.style.setProperty('--theme-color', '#1cb65d');
+};
+buttons[3].onclick = function () {
+  theme.style.setProperty('--theme-color', '#8e44ad');
+};
+buttons[4].onclick = function () {
+  theme.style.setProperty('--theme-color', '#f4b932');
+};
